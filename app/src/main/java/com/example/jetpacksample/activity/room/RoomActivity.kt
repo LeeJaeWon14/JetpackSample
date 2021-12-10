e7d19c746a74fb744aa5d2f2e5e2832bdbc438e1
LeeJaeWon <zhtm2@openobject.net>
2021-12-10 18:06:02 +0900

    - Room sample modify

--- app/src/main/java/com/example/jetpacksample/activity/room/RoomActivity.kt
+++ app/src/main/java/com/example/jetpacksample/activity/room/RoomActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.jetpacksample.databinding.ActivityRoomBinding
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class RoomActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRoomBinding
@@ -19,22 +15,38 @@ class RoomActivity : AppCompatActivity() {
         // UI init.
         binding.apply {
             btnRoom.setOnClickListener {
-                Snackbar.make(it, "저장되었습니다.", Snackbar.LENGTH_SHORT).show()
-                CoroutineScope(Dispatchers.IO).launch {
+                Thread {
+                    Snackbar.make(it, "저장되었습니다.", Snackbar.LENGTH_SHORT).show()
+
                     val dao = MyRoomDatabase.getInstance(this@RoomActivity).getRoomDAO()
                     dao.insertRoom(
                         RoomEntity(0, edtRoom.text.toString())
                     )
 
                     val entityList = dao.selectRoom()
-                    withContext(Dispatchers.Main) {
+                    runOnUiThread {
                         tvRoom.text = ""
                         entityList.forEach {
                             tvRoom.text = tvRoom.text.toString().plus("\r\n ${it.testValue}")
                         }
                     }
-                }
+                }.start()
             }
         }
+
+        //                CoroutineScope(Dispatchers.IO).launch {
+//                    val dao = MyRoomDatabase.getInstance(this@RoomActivity).getRoomDAO()
+//                    dao.insertRoom(
+//                        RoomEntity(0, edtRoom.text.toString())
+//                    )
+//
+//                    val entityList = dao.selectRoom()
+//                    withContext(Dispatchers.Main) {
+//                        tvRoom.text = ""
+//                        entityList.forEach {
+//                            tvRoom.text = tvRoom.text.toString().plus("\r\n ${it.testValue}")
+//                        }
+//                    }
+//                }
     }
 }
